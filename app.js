3ea85ddc9b012d9d51d33842ba887c0f28c31121
₳₥J₳Đł₥Đ₳Đ <amjadabro936@gmail.com>
2024-08-02 06:58:59 -0700

    Update: Changed The Api

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const api_url = "https://api.quotable.io/random";
+const api_url = "https://zenquotes.io/api/random";
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 
@@ -7,8 +7,8 @@ async function getQuote(url) {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
-    quote.innerText = data.content;
-    author.innerText = data.author;
+    quote.innerText = data.q;
+    author.innerText = data.a;
   } catch (error) {
     console.error("Error fetching quote:", error);
     quote.innerText = "Sorry, couldn't fetch a quote.";
